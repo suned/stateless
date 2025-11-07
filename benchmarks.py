4c0695f87703c2cf013f97e193f795953dd40c3c
Sune Debel <1228354+suned@users.noreply.github.com>
2025-11-07 21:03:03 +0100

    add benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -1,9 +1,9 @@
+# ruff: noqa: D100, D103
+
 from functools import reduce
-from typing import Callable
+from typing import Any, Callable
 
 from pytest_benchmark.fixture import BenchmarkFixture
-from typing_extensions import Never
-
 from stateless import (
     Ability,
     Depend,
@@ -13,53 +13,24 @@ from stateless import (
     handle,
     need,
     run,
-    success,
     supply,
 )
 from stateless.errors import UnhandledAbilityError
+from typing_extensions import Never
 
 
-def create_effect_chain(chain_length: int) -> Callable[[], Success[None]]:
-    """Creates a chain using functools.reduce."""
-
-    def yield_from(
-        f: Callable[[], Success[None]],
-    ) -> Callable[[], Success[None]]:
-        def _() -> Success[None]:
-            result = yield from f()
-            return result
-
-        return _
-
-    # Base function
-    def base_function() -> Success[None]:
-        result = yield from success(None)
-        return result
-
-    # Create chain using reduce
-    return reduce(
-        lambda acc, _: yield_from(acc), range(chain_length - 1), base_function
-    )
-
-
-def test_effect_chain(benchmark: BenchmarkFixture) -> None:
-    """Benchmark a long chain of functions using functools.reduce."""
-
-    effect = create_effect_chain(500)()
-    benchmark(run, effect)
-
-
-def never_handler(_: Ability) -> Never:
+def never_handler(_: Ability[Any]) -> Never:
     raise UnhandledAbilityError()
 
 
-dummy_handler: Handler[Never] = handle(never_handler)
+dummy_handler: Handler[Never] = handle(never_handler)  # type: ignore
 
 
-def create_handler_chain(chain_length: int) -> Callable[[], Success[str]]:
+def create_effect_chain(chain_length: int, n_yields: int) -> Callable[[], Success[str]]:
     def base() -> Depend[Need[str], str]:
-        s = yield from need(str)
-        return s
+        for i in range(n_yields):
+            yield from need(str)
+        return "done"
 
     def wrap_test_handler(
         f: Callable[[], Depend[Need[str], str]],
@@ -71,7 +42,14 @@ def create_handler_chain(chain_length: int) -> Callable[[], Success[str]]:
     return supply("")(g)
 
 
+def test_effect_chain(benchmark: BenchmarkFixture) -> None:
+    """Benchmark a long chain of functions using functools.reduce."""
+
+    effect = create_effect_chain(500, 0)()
+    benchmark(run, effect)
+
+
 def test_handler_chain(benchmark: BenchmarkFixture) -> None:
-    effect = create_handler_chain(500)()
+    effect = create_effect_chain(chain_length=500, n_yields=500)()
 
     benchmark(run, effect)
